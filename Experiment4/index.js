d816321ad46ca0f7a1571ac31ff4a96f056e848f
Jisha Anna Philip <jishaannphilip@gmail.com>
2021-09-28 09:37:36 +0530

    Assignment

--- Experiment4/index.js
+++ Experiment4/index.js
@@ -1,30 +1,31 @@
 function validateForm() {
     // Validation
-    const formComponent = [ 
-        document.forms["buyForm"]["tagline"].value, 
-        document.forms["buyForm"]["color"].value, 
-        document.forms["buyForm"]["size"].value,  
+    const formComponent = [
+        document.forms["buyForm"]["tagline"].value,
+        document.forms["buyForm"]["color"].value,
+        document.forms["buyForm"]["size"].value,
         document.forms["buyForm"]["date"].value,
     ];
-    
-    const isEmpty = Object.values(formComponent).some( (element) => element === "");
-    
-    console.log(formComponent) 
+    console.log("asdfasdfasdf", formComponent)
+    const isEmpty = Object.values(formComponent).some((element) => element === "");
+    console.log(formComponent)
+
     if (isEmpty) {
-        alert("Please fill in all fields"); 
+        alert("Please fill in all fields");
         return;
     }
-    
+
     // Bill
-    const bill = `RECEIPT \nTagline: ${formComponent[0]}\nColor: ${formComponent[1]}\nSize: ${formComponent[2]}  \nDate: ${formComponent[3]}`;
-    
+    const bill = `RECEIPT \nTagline: ${formComponent[0]}\nColor:
+    ${formComponent[1]}\nSize: ${formComponent[2]} \nDate:
+    ${formComponent[3]}`;
     alert(bill);
-    }
-    
-    const form = document.getElementById("buyForm"); 
-    console.log(form)
- 
-    form.addEventListener("submit", (event) => {
-        // stop form submission 
-        event.preventDefault();
-    });
+}
+
+const form = document.getElementById("buyForm");
+console.log(form)
+
+form.addEventListener("submit", (event) => {
+    // stop form submission
+    event.preventDefault();
+});
